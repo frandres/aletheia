f07ea88dfdce0ce1de8dfc4da4c443f2d46761a3
diegoolano <diegoolano@gmail.com>
2015-03-05 12:42:37 +0100

    updates to get keywords for politician text snippets

--- get_keywords.py
+++ get_keywords.py
@@ -1,46 +1,47 @@
 from elasticsearch import Elasticsearch
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from math import log
 
 
 def get_idf(word):
     es = Elasticsearch()
-
     q = {'query': {'bool': {'disable_coord': True, 'must': [{'match_phrase': {'body': {'analyzer': 'analyzer_keywords', 'query': word}}}]}}}
-
     count = es.count(index='texnews_english',body =q)['count']
+    N = es.count(index='texnews_english')['count']
+    #print "count: "+str(N)
+    #print word,count
+    return log(float(N)/(1+count))
 
-    return log(1+count)
-
-def get_keywords(documents, min_freq=3.0, min_ngrams = 1, max_ngrams = 3):
-
-    tfidf_v = CountVectorizer(ngram_range=(min_ngrams,max_ngrams),stop_words = english, tokenizer = tokenizer_with_stemming, min_df = min_freq/len(documents))
-
+def get_keywords(documents, min_freq=3.0, min_ngrams = 1, max_ngrams = 3):   
+    
+    minf = float(min_freq)/len(documents)
+    tfidf_v = CountVectorizer(ngram_range=(min_ngrams,max_ngrams),stop_words = "english", min_df = minf)
     t = tfidf_v.fit_transform(documents)
+    
     features = tfidf_v.get_feature_names()
-
     matrix = t.todense()
-
     keywords = {}
-
+    
     for i in range(len(documents)):
         # Compute the norm and the weight of each feature.
-
         for j in range(len(features)):
             freq = matrix[i,j]
 
             idf = get_idf(features[j])
             tf_idf_w = idf*freq
-
+            
+            #print features[j], idf
             if features[j] in keywords:
-                keywords[features[j]]+=1
+                keywords[features[j]]+=tf_idf_w
             else:
-                keywords[features[j]]=1
+                keywords[features[j]]=tf_idf_w
                  
     sorted_keywords = [(y,x) for (x,y) in keywords.items()]
     sorted_keywords.sort(reverse= True)
 
     return sorted_keywords
 
-example = ['Diego is in favor of drug legalization', 'Diego said: "Medical Marihuana is a right', 'We believe that drug legalization could lead to the use of medical marihuana']
+example = ['Diego is in favor of drug legalization', 'Diego said: "Medical Marijuana is a right', 'We believe that drug legalization could lead to the use of medical marijuana',
+           'Diego is in favor of drug legalization', 'Diego said: "Medical Marijuana is a right', 'We believe that drug legalization could lead to the use of medical marijuana']
 
-print get_keywords(example)[0:10]
+print get_keywords(example)
